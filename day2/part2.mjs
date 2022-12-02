280b11b146ca6be69a6f7c788ac8bb05e105df01
Lietze Diderich <lietze14@gmail.com>
2022-12-03 00:09:23 +0100

    day2 part2 done :D

---
+++ day2/part2.mjs
@@ -0,0 +1,27 @@
+import * as fs from 'fs';
+
+const extra = {'X': 1, 'Y': 2, 'Z': 3};
+function rework(fst, snd) {
+    let win = (fst == 'A') ? 'Y' : (fst == 'B') ? 'Z' : 'X';
+    let lose = (fst == 'A') ? 'Z' : (fst == 'B') ? 'X' : 'Y';
+    let draw = (fst == 'A') ? 'X' : (fst == 'B') ? 'Y' : 'Z';
+    return (snd == 'X') ? lose : (snd == 'Y') ? draw : win;
+}
+function won(fst, snd) {
+    let X = (fst == 'A') ? 3 : (fst == 'B') ? 0 : 6;
+    let Y = (fst == 'A') ? 6 : (fst == 'B') ? 3 : 0;
+    let Z = (fst == 'A') ? 0 : (fst == 'B') ? 6 : 3;
+    return (snd == 'X') ? X : (snd == 'Y') ? Y : Z;
+}
+
+fs.readFile('input.txt', 'utf8', (err, data) => {
+    let newdata = data.split('\n');
+    let score = 0;
+    newdata.forEach(item => {
+        let fst = item[0];
+        let snd = item[2];
+        snd = rework(fst, snd);
+        score += won(fst, snd) + extra[snd];
+    });
+    console.log('total:', score);
+});
