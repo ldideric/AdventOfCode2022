b98c2bafc8d60791f12b1d5beca8d6f52d2e1c2d
Lietze Diderich <lietze14@gmail.com>
2022-12-05 13:25:04 +0100

    day3 part2 done

--- day03/part2.mjs
+++ day03/part2.mjs
@@ -1,29 +1,34 @@
 import * as fs from 'fs';
 
-let splitdata = [], match = [], total = 0;
+function isLowerCase(char) {
+    return (char == String(char).toLowerCase());
+}
 
-fs.readFile('input.txt', 'utf8', (err, data) => {
-    data = data.split('\n');
+let splitData = [], splitIndex = 0, matchedCharVal = [], total = 0;
 
-    for (let dataindex = 0; dataindex < data.length; dataindex++) {
-        for (let three = 0; three < 3; three++) {
-            splitdata[dataindex] = 0;
-            splitdata[dataindex] += data[dataindex * 3 + three];
+fs.readFile('input.txt', 'utf8', (err, rawData) => {
+    rawData = rawData.split('\n');
+    rawData.forEach((string, index) => {
+        rawData[index] = string.replace('\r', '');
+    });
+
+    for (let dataIndex = 0; dataIndex < rawData.length; splitIndex++) {
+        for (let three = 0; three < 3; three++, dataIndex++) {
+            splitData[splitIndex] = splitData[splitIndex] || [];
+            splitData[splitIndex][three] = rawData[dataIndex];
         }
     }
-    console.log(splitdata);
 
-    // splitdata.forEach((item, i) => {
-    //     for (let j = 0; j < item[0].length; j++) {
-    //         if (item[1].match(item[0][j])) {
-    //             match[i] = item[1].match(item[0][j]);
-    //             match[i] = (match[i][0].charCodeAt(0) - ((match[i][0] == String(match[i][0]).toLowerCase()) ? 96 : 38));
-    //         }
-    //     }
-    // });
+    splitData.forEach(([matchingString, str1, str2]) => {
+        for (let i = 0; i < matchingString.length; i++) {
+            if (str1.match(matchingString[i]) && str2.match(matchingString[i])) {
+                matchedCharVal[matchedCharVal.length] =
+                    (matchingString[i].charCodeAt(0) - ((isLowerCase(matchingString[i])) ? 96 : 38));
+                break;
+            }
+        }
+    });
 
-    // match.forEach(element => {
-    //     total += element;
-    // });
-    // console.log(total);
+    matchedCharVal.forEach(value => { total += value; });
+    console.log(total);
 });
