7868b5c20230e0bf97c20359956e2ca534c4f0ae
Lietze Diderich <lietze14@gmail.com>
2022-12-05 14:52:27 +0100

    day4 finished

---
+++ day4/part2.mjs
@@ -0,0 +1,29 @@
+import * as fs from 'fs';
+
+function RangeToInt(range) {
+    range = range.split('-');
+    range = [Number(range[0]), Number(range[1])];
+    return range;
+}
+
+function containsRange(one, two) {
+    return ((one[0] == two[0] || one[1] == two[1])
+        || (one[0] > two[0] && one[0] <= two[1])
+        || (two[0] > one[0] && two[0] <= one[1]));
+}
+
+let splitData = [], total = 0;
+
+fs.readFile('input.txt', 'utf8', (err, rawData) => {
+    rawData = rawData.split('\n');
+    rawData.forEach((string, i) => {
+        splitData[i] = string.replace('\r', '').split(',');
+    });
+
+    splitData.forEach(ranges => {
+        ranges = [RangeToInt(ranges[0]), RangeToInt(ranges[1])];
+        total += containsRange(ranges[0], ranges[1]);
+    });
+
+    console.log(total);
+});
